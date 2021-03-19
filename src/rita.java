164e826500dab2ab729c7b65a94b98978634c9dd
TTmesb <TTmesb@users.noreply.github.com>
2021-03-19 10:58:22 +0100

    ja

--- src/rita.java
+++ src/rita.java
@@ -1,20 +1,71 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
-public class rita extends Canvas {
-    public rita(){
+public class rita extends Canvas implements Runnable{
+    private static boolean körs;
+    private int houseX, houseY;
+   private int houseVX, houseVY;
+
+   private int width = 600;
+   private int height = 400;
+
+   private int treeX,treeY;
+
+   private Thread tråd;
+   int fps = 30;
+
+   private BufferStrategy bs;
+   //private BufferedImage bild;
+
+   public rita(){
         JFrame rityta = new JFrame("EN SUPERDUPER COOL BILD");
-        this.setSize(750,750);
+        this.setSize(width,height);
         rityta.add(this);
         rityta.pack();
         rityta.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         rityta.setVisible(true);
+
+        körs = false;
+
+        houseX = 150;
+        houseY = 200;
+        houseVX = 1;
+        houseVY = 0;
     }
-    public void paint(Graphics p) {         // vilka former som ska ritas
-        drawHouse(p, 200,300);
+    public void draw () {         // vilka former som ska ritas
+        bs = getBufferStrategy();
+        if (bs == null){
+            createBufferStrategy(4);
+            return;
+        }
+        Graphics p = bs.getDrawGraphics();
+
+       update();
+       p.setColor(Color.white);
+       p.fillRect(0,0,width,height);
+        drawHouse(p, houseX,houseY);
+
         drawTree(p, 100, 100);
-
-
+        drawTree(p, 100, 111);
+        drawTree(p, 100, 122);
+        drawTree(p, 100, 133);
+        drawTree(p, 100, 144);
+        drawTree(p, 100, 155);
+        p.dispose();
+        bs.show();
+    }
+    public void update() {
+        houseX += houseVX;
+        if (houseX > width) {
+            houseVX = -1;
+        }
+        if (houseX < 0){
+            houseVX = 1;
+        }
     }
 
     private void drawTree(Graphics p, int y, int x) {
@@ -39,5 +90,53 @@ public class rita extends Canvas {
 
     public static void main(String[] args) {
         rita bild = new rita();
+        körs = true;
+        bild.start();
+    }
+    public synchronized void start(){
+       tråd = new Thread(this);
+       tråd.start();
+    }
+
+    public synchronized void stop(){
+    körs = false;
+        try {
+            tråd.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        double deltaT = 1.0/fps;
+        long lastTime = System.currentTimeMillis();
+
+        while(körs){
+            long now = System.currentTimeMillis();
+            if (now-lastTime > deltaT){
+                update();
+                draw();
+                lastTime=now;
+            }
+        }
+    }
+    private  class KL implements KeyListener{
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if(KeyEvent.getKeyChar()== 'a');
+            treeX-= 5;
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
     }
 }
